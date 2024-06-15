b5a0d8c0e5d9927aa6bae8dda9938ef872681793
Bhautik Sangani <161152146+bhautik124@users.noreply.github.com>
2024-06-15 20:14:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const navLinks = document.querySelectorAll('#nav #right a[href^="#"]');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
+const nav = document.getElementById('nav');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+});
